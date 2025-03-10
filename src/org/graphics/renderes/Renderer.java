a343f5080f143e3829c3718bbfcb623f739a9a17
Mihail Georgiev <mmisho0501@gmail.com>
2025-03-10 11:47:45 +0200

    Lab 4 - Circle going around corners

--- src/org/graphics/renderes/Renderer.java
+++ src/org/graphics/renderes/Renderer.java
@@ -1,81 +1,65 @@
 package org.graphics.renderes;
 
-import static org.lwjgl.opengl.GL30.*;
-
-import java.nio.FloatBuffer;
-
+import org.graphics.renderes.shapes.CircleRenderer;
 import org.graphics.renderes.shapes.ShapeRenderer;
 import org.graphics.utils.ShaderProgram;
 import org.joml.Matrix4f;
-import org.lwjgl.BufferUtils;
 
 
 public class Renderer {
-    @SuppressWarnings("FieldCanBeLocal")
-    private int vaoID, vboID;
     private ShaderProgram shaderProgram;
-//    private ShapeRenderer[] shapes;
+    private ShapeRenderer[] shapes;
 
     public void init() {
         shaderProgram = new ShaderProgram("res/shaders/vertexShader.vert", "res/shaders/fragmentShader.frag");
 
-        float[] vertices = {
-                // X, Y, Z      R, G, B
-                -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // Червен връх
-                0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // Зелен връх
-                0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f   // Син връх
+        shapes = new ShapeRenderer[]{
+                new CircleRenderer(0.0f, 0.0f, 0.1f, 100, new float[]{1f, 1f, 0f}, new float[]{1f, 1f, 0f}),
         };
-
-        vaoID = glGenVertexArrays();
-        glBindVertexArray(vaoID);
-
-        vboID = glGenBuffers();
-        glBindBuffer(GL_ARRAY_BUFFER, vboID);
-
-        FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
-        verticesBuffer.put(vertices).flip();
-        glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
-
-        // Атрибут 0 - Позиция (vec3)
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, 0);
-        glEnableVertexAttribArray(0);
-
-        // Атрибут 1 - Цвят (vec3)
-        glVertexAttribPointer(1, 3, GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
-        glEnableVertexAttribArray(1);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-
     }
 
+    private float translateX = 0.0f;
+    private float translateY = 0.5f;
+    private float speed = 0.0005f;
 
     public void render() {
         shaderProgram.use();
 
+        if(translateX < 0.5f && translateY >= 0.5f) {
+            translateX += speed;
+        }
+
+        if(translateX >= 0.5f && translateY > -0.5f) {
+            translateY -= speed;
+        }
+
+        if(translateY <= -0.5f && translateX > -0.5f) {
+            translateX -= speed;
+        }
+
+        if(translateX <= -0.5f && translateY < 0.5f) {
+            translateY += speed;
+        }
+
         Matrix4f modelMatrix = new Matrix4f();
-        modelMatrix
-                .identity()
-                .scale(1.2f, 0.7f, 0f)
-                .translate(0.3f, 0.9f, 0f);
+        modelMatrix.identity().translate(translateX, translateY, 0f);
 
         shaderProgram.setUniform("modelMatrix", modelMatrix);
 
-        glPointSize(10.0f);
-        glBindVertexArray(vaoID);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
+        for (ShapeRenderer shape : shapes) {
+            shape.render();
+        }
     }
 
     public void cleanup() {
-//        for (ShapeRenderer shape : shapes) {
-//            shape.cleanup();
-//        }
+        for (ShapeRenderer shape : shapes) {
+            shape.cleanup();
+        }
     }
 
     public void updateAspectRation(int width, int height) {
-//        for (ShapeRenderer shape : shapes) {
-//            shape.updateAspectRation(width, height);
-//        }
+        for (ShapeRenderer shape : shapes) {
+            shape.updateAspectRation(width, height);
+        }
     }
 }
