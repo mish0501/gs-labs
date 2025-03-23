2b95f138d3daa2f4ca92b3446ef35246b1452916
Mihail Georgiev <mmisho0501@gmial.com>
2025-03-23 12:40:51 +0200

    Lab 6 - Task 4.2

--- src/org/graphics/renderes/Renderer.java
+++ src/org/graphics/renderes/Renderer.java
@@ -9,11 +9,12 @@ import org.graphics.renderes.shapes.ShapeRenderer;
 import org.graphics.utils.ShaderProgram;
 import org.joml.Matrix4f;
 import org.lwjgl.BufferUtils;
+import org.lwjgl.system.MemoryUtil;
 
 
 public class Renderer {
     @SuppressWarnings("FieldCanBeLocal")
-    private int vaoID, vboID;
+    private int vaoID, vboID, eboID;
     private ShaderProgram shaderProgram;
 //    private ShapeRenderer[] shapes;
 
@@ -21,24 +22,27 @@ public class Renderer {
         shaderProgram = new ShaderProgram("res/shaders/vertexShader.vert", "res/shaders/fragmentShader.frag");
 
         float[] vertices = {
-                0f, 0.8f, 0f, 0f, 0.8f, 0f,
-                -0.4f, 0.4f, 0f, 0f, 0.8f, 0f,
-                0.4f, 0.4f, 0f, 0f, 0.8f, 0f,
+                -0.6f, 0.8f, 0f, 0.52f, 0.42f, 0f,
+                -0.25f, 0.5f, 0f, 0.2f, 0.4f, 0.5f,
+                -0.05f, 0f, 0f, 0.2f, 0.4f, 0.5f,
+                -0.05f, -0.8f, 0f, 0.2f, 0.4f, 0.25f,
+                -0.4f, -0.9f, 0f, 0.52f, 0.42f, 0f,
+                0.6f, 0.8f, 0f, 0.52f, 0.42f, 0f,
+                0.25f, 0.5f, 0f, 0.2f, 0.4f, 0.5f,
+                0.05f, 0f, 0f, 0.2f, 0.4f, 0.5f,
+                0.05f, -0.8f, 0f, 0.2f, 0.4f, 0.25f,
+                0.4f, -0.9f, 0f, 0.52f, 0.42f, 0f,
+        };
 
-                0f, 0.6f, 0f, 0f, 0.7f, 0f,
-                -0.6f, 0f, 0f, 0f, 0.7f, 0f,
-                0.6f, 0f, 0f, 0f, 0.7f, 0f,
-
-                0f, 0.2f, 0f, 0f, 0.6f, 0f,
-                -0.7f, -0.4f, 0f, 0f, 0.6f, 0f,
-                0.7f, -0.4f, 0f, 0f, 0.6f, 0f,
-
-                -0.1f, -0.4f, 0f, 0.4f, 0.2f, 0f,
-                -0.1f, -0.7f, 0f, 0.4f, 0.2f, 0f,
-                0.1f, -0.7f, 0f, 0.4f, 0.2f, 0f,
-                0.1f, -0.7f, 0f, 0.4f, 0.2f, 0f,
-                0.1f, -0.4f, 0f, 0.4f, 0.2f, 0f,
-                -0.1f, -0.4f, 0f, 0.4f, 0.2f, 0f,
+        int[] indices = {
+                0, 1, 5,
+                5, 1, 6,
+                1, 2, 6,
+                6, 2, 7,
+                2, 3, 7,
+                7, 3, 8,
+                3, 4, 8,
+                8, 4, 9
         };
 
         vaoID = glGenVertexArrays();
@@ -50,6 +54,13 @@ public class Renderer {
         FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
         verticesBuffer.put(vertices).flip();
         glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+
+
+        IntBuffer indexBuffer = MemoryUtil.memAllocInt(indices.length);
+        indexBuffer.put(indices).flip();
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer, GL_STATIC_DRAW);
 
         // Атрибут 0 - Позиция (vec3)
         glVertexAttribPointer(0, 3, GL_FLOAT, false, 6 * Float.BYTES, 0);
@@ -70,8 +81,8 @@ public class Renderer {
         shaderProgram.setUniform("modelMatrix", modelMatrix);
 
         glBindVertexArray(vaoID);
-        glDrawArrays(GL_TRIANGLES, 0, 9);
-        glDrawArrays(GL_TRIANGLE_STRIP, 9, 6);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
     }
 
